ea4bfcf664b0c4903597b13869fa44a40480c8cd
reinhold1996 <54431389+reinhold1996@users.noreply.github.com>
2023-04-19 10:50:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 """
 Diese Datei sollte eine Konsole ergeben, die unter Linux in einem Screen ausgeführt werden kann. Sie ist als Dienst nicht in verwendung.
+Die Funktion foreground() ist dafür verantwortlich, den Hotfolder zu starten und eine Benutzeroberfläche bereitzustellen, um den Benutzern verschiedene Befehle zur Steuerung des Hotfolders zur Verfügung zu stellen.
+
+Die Funktion startet den isrunning-Thread, der für die Überwachung des Hotfolders und die Ausführung von Aktionen bei Änderungen verantwortlich ist. Danach werden verschiedene Befehle für den Benutzer angezeigt und er wird aufgefordert, eine Eingabe zu tätigen.
+
+Wenn der Benutzer den Befehl "help" eingibt, wird eine Liste von Befehlen angezeigt. Wenn der Benutzer den Befehl "start" eingibt, wird der Hotfolder gestartet, wenn er noch nicht ausgeführt wird. Wenn er bereits läuft, wird eine entsprechende Meldung ausgegeben. Wenn der Benutzer den Befehl "stop" eingibt, wird der Hotfolder gestoppt, wenn er läuft. Wenn er nicht läuft, wird eine entsprechende Meldung ausgegeben. Wenn der Benutzer den Befehl "status" eingibt, wird der Status des Hotfolders ausgegeben. Wenn der Benutzer den Befehl "exit" eingibt, wird das Programm beendet.
+
+Wenn der Benutzer den Befehl "generate" eingibt, werden zwei Module importiert, programm.first_run.service und programm.first_run.startsh, die für die Erstellung einer Servicedatei und einer .sh-Startdatei verantwortlich sind.
+
+Insgesamt dient die Funktion foreground() dazu, den Benutzer zu unterstützen und die Steuerung des Hotfolders zu erleichtern.
 """
 
 
